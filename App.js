88486c63f7381a9daa546d663bc730778ff00511
Your GitHub Username <anugrahalifiandi20@gmail.com>
2024-07-01 22:46:03 +0800

    g

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react' 
-import { Colors } from 'react-native/Libraries/NewAppScreen'
-const App = () => {
-  return (
-  <View style={{flex:1}}>
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: 'green' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'yellow' }}></View>
-    </View>
-    <View style={{ flex: 1, flexDirection: "column" }}>
-      <View style={{ flex: 1, backgroundColor: 'red' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'blue' }}></View>
-    </View>
-    </View>
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-  )
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Anugrah Alifiandi</Text>
+      <Text style={{
+        fontSize: 20,
+        fontStyle: 'Times New Roman',
+        textDecorationLine: 'underline',
+        color: 'black',       
+      }}> Text Ukuran 20!</Text>
+      <Text style={{
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'white'
+      }}>Anugrah Alifiandi </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
-export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
